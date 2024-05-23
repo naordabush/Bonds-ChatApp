2e61bb845f07733b161d4b783c55567cd1336e00
OLD-INTERLEM\naor.dabush <naor.dabush@interlem.it>
2024-05-23 15:07:36 +0200

    first commit

--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -3,7 +3,7 @@ import { BsEmojiSmileFill } from "react-icons/bs";
 import { IoMdSend } from "react-icons/io";
 import styled from "styled-components";
 import Picker from "emoji-picker-react";
-\
+
 export default function ChatInput({ handleSendMsg }) {
   const [msg, setMsg] = useState("");
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
